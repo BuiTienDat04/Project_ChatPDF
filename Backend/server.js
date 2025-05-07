e4fe5d25c6e86f1b08ee5ad9f4488f993f40a423
Manhpro04 <166919626+Manhpro04@users.noreply.github.com>
2025-05-07 22:28:40 +0700

    ManhcommitBackend

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,13 @@
-const { createServer } = require('node:http');
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 
-const hostname = 'localhost';
-const port = 3000;
 
-const server = createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
